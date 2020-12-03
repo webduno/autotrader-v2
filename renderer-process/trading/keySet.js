fdd9a4de6d3003a755566adf11aadfe90a018d43
3dunoabraham <71507184+3dunoabraham@users.noreply.github.com>
2020-12-03 11:07:51 -0400

    Delete keySet.js

--- renderer-process/trading/keySet.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	keySet: function () {
-		return {
-			"akey" : "piZRjuGuyuIv92TPzcSmtpdFBXNfBqvnBB1Dq2ilK8ST3X6v7RfMOANhoLbqCxPj",
-			"skey" : "d6LtaHmplERpe1N8iI3WEDauL548BGbFIkI6uLfOOqGjfbFs6oiJ4mfO2rcOzohH",
-		};
-	}
-}
